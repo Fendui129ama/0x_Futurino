e7dbb86a8b00f0853ada5c40e8b1c4bc699a4f4c
Fendui129ama <adesutsna21@gmail.com>
2026-04-14 17:42:17 +0200

    Update main.sol

--- main.sol
+++ main.sol
@@ -71,3 +71,76 @@ library FuturinoSet {
 
     struct AddressSet {
         address[] _items;
+        mapping(address => uint256) _posPlusOne;
+    }
+
+    function contains(AddressSet storage s, address a) internal view returns (bool) {
+        return s._posPlusOne[a] != 0;
+    }
+
+    function length(AddressSet storage s) internal view returns (uint256) {
+        return s._items.length;
+    }
+
+    function at(AddressSet storage s, uint256 idx) internal view returns (address) {
+        if (idx >= s._items.length) revert FuturinoSet__IndexOOB();
+        return s._items[idx];
+    }
+
+    function add(AddressSet storage s, address a) internal returns (bool) {
+        if (a == address(0)) return false;
+        if (s._posPlusOne[a] != 0) return false;
+        s._items.push(a);
+        s._posPlusOne[a] = s._items.length;
+        return true;
+    }
+
+    function remove(AddressSet storage s, address a) internal returns (bool) {
+        uint256 p = s._posPlusOne[a];
+        if (p == 0) return false;
+        uint256 idx = p - 1;
+        uint256 last = s._items.length - 1;
+        if (idx != last) {
+            address swap = s._items[last];
+            s._items[idx] = swap;
+            s._posPlusOne[swap] = idx + 1;
+        }
+        s._items.pop();
+        delete s._posPlusOne[a];
+        return true;
+    }
+}
+
+library FuturinoECDSA {
+    error FuturinoECDSA__BadSig();
+    error FuturinoECDSA__BadV();
+    error FuturinoECDSA__BadS();
+
+    // secp256k1n/2
+    uint256 internal constant _HALF_ORDER =
+        0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0;
+
+    function recover(bytes32 digest, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        if (v != 27 && v != 28) revert FuturinoECDSA__BadV();
+        if (uint256(s) > _HALF_ORDER) revert FuturinoECDSA__BadS();
+        address signer = ecrecover(digest, v, r, s);
+        if (signer == address(0)) revert FuturinoECDSA__BadSig();
+        return signer;
+    }
+}
+
+abstract contract FuturinoReentrancyGuard {
+    error FuturinoReentrancyGuard__Reentered();
+
+    uint256 private _rg;
+
+    modifier nonReentrant() {
+        if (_rg == 2) revert FuturinoReentrancyGuard__Reentered();
+        _rg = 2;
+        _;
+        _rg = 1;
+    }
+
+    constructor() {
+        _rg = 1;
+    }
